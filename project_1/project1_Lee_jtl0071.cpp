74a399859adea299f2bed0aa077a639277b8abe2
Jordan Lee <jtlee@rmgj2.com>
2024-08-25 13:33:23 -0500

    init commit

---
+++ project_1/project1_Lee_jtl0071.cpp
@@ -0,0 +1,21 @@
+/*
+Jordan Lee
+jtl0071
+project1_Lee_jtl0071.cpp
+
+Compile Instructions:
+run 'g++ project1_Lee_jtl0071.cpp -o project1_Lee_jtl0071'
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Take Loan Amount
+    int loanAmt;
+    cout << "Loan Amount: ";
+    cin >> loanAmt;
+
+
+    return 0;
+}
