b61c3d147e4087d12bff4f05498f2003cd3683e7
Jordan Lee <jtlee@rmgj2.com>
2024-08-27 15:08:40 -0500

    Finishing touches, edge cases

--- project_1/project1_Lee_jtl0071.cpp
+++ project_1/project1_Lee_jtl0071.cpp
@@ -7,16 +7,13 @@ Compile and Run Instructions:
 compile 'g++ project1_Lee_jtl0071.cpp -o project1_Lee_jtl0071.out'
 execute: './project1_Lee_jtl0071.out'
 */
-
 #include <iostream>
 #include <iomanip>
-
 using namespace std;
 
 int main()
 {
     // Create util variables
-    float runningTotal = 0;
     float principal;
     int monthsCount = 0;
     float interestAmt;
@@ -30,20 +27,34 @@ int main()
     // Take User Input, handle errors with input
     cout << "Loan Amount: ";
     cin >> loanAmt;
-    if (!cin){
+    if (!cin)
+    {
         throw invalid_argument("Input must be a float"); // Input error handling from Painless Programming on Youtube
     }
     cout << "Interest Rate (% per year): ";
     cin >> interestRate;
-    if (!cin){
+    if (!cin)
+    {
         throw invalid_argument("Input must be a float");
     }
     cout << "Monthly Payments: ";
     cin >> monthlyPayment;
-    if (!cin){
+    cout << endl;
+    if (!cin)
+    {
         throw invalid_argument("Input must be a integer");
     }
 
+    interestAmt = loanAmt * ((interestRate / 12) / 100);
+    if (monthlyPayment < interestAmt)
+    {
+        cout << "ERROR: Interest is more than your payment. Please enter a monthly payment higher than " << interestAmt << ": ";
+        cin >> monthlyPayment;
+        if (!cin)
+        {
+            throw invalid_argument("Input must be a float");
+        }
+    }
 
     // Output Header
     cout << "**********************************************************" << endl;
@@ -52,8 +63,8 @@ int main()
     cout.setf(ios::left, ios::adjustfield);
     cout << setw(7) << "Month" << setw(10) << "Balance" << setw(13) << "Payment"
          << setw(8) << "Rate" << setw(11) << "Interest" << setw(9) << "Principal" << endl;
-    // First Line Output
 
+    // First Line Output, do separate from loop due to N/As
     cout << setprecision(2) << fixed; // Sets float precision to 2 decimal places, because money
     cout << setw(7) << "0" << "$"
          << setw(9) << loanAmt
@@ -63,22 +74,24 @@ int main()
          << setw(9) << "N/A"
          << endl;
 
-    runningTotal = loanAmt;
-
-    while (runningTotal > 0)
+    // Loop until loan is completely paid off
+    while (loanAmt > 0)
     {
+        
         monthsCount++;
+
         // Calculate Interest, Principal, Payment
-        interestAmt = runningTotal * ((interestRate / 12) / 100);
-        if (monthlyPayment > runningTotal)
+        interestAmt = loanAmt * ((interestRate / 12) / 100);
+        if (monthlyPayment > loanAmt)
         {
-            monthlyPayment = runningTotal + interestAmt;
+            monthlyPayment = loanAmt + interestAmt;
         }
-        // FIXME: Make sure monthly payments are LARGER than monthly interest, or else app explodes 
         principal = monthlyPayment - interestAmt;
-        runningTotal -= principal;
+        loanAmt -= principal;
+
+
         cout << setw(7) << monthsCount << "$"
-             << setw(9) << runningTotal << "$"
+             << setw(9) << loanAmt << "$"
              << setw(12) << monthlyPayment
              << setw(8) << interestRate / 12 << "$"
              << setw(10) << interestAmt << "$"
@@ -88,8 +101,8 @@ int main()
         totalInterest += interestAmt;
     }
 
-    cout << "**********************************************************" << endl << endl;
-
+    cout << "**********************************************************" << endl
+         << endl;
     cout << "It takes " << to_string(monthsCount) << " months to pay off the loan" << endl;
     cout << "Total interest paid is: $" << totalInterest << endl;
 
