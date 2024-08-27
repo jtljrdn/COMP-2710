525c4f82f86a7d82d05c0bad45db716c2ffe3efb
Jordan Lee <jtlee@rmgj2.com>
2024-08-26 23:13:29 -0500

    Input, Error Handling, output setup

--- project_1/project1_Lee_jtl0071.cpp
+++ project_1/project1_Lee_jtl0071.cpp
@@ -8,24 +8,41 @@ run 'g++ project1_Lee_jtl0071.cpp -o project1_Lee_jtl0071'
 */
 
 #include <iostream>
+#include <iomanip> 
 using namespace std;
 
 int main(){
+    // Create util variables
+
+
     // Create input variables
     int loanAmt;
-    int interestRate;
+    float interestRate;
     int monthlyPayments;
 
-    // Input
+    // Take User Input, handle errors with input
     cout << "Loan Amount: ";
     cin >> loanAmt;
-
+    if (!cin){
+        throw invalid_argument("Input must be a integer"); // Input error handling from Painless Programming on Youtube
+    }
     cout << "Interest Rate (% per year): ";
     cin >> interestRate;
-
-    cout << "Monthly Payments :";
+    if (!cin){
+        throw invalid_argument("Input must be a float");
+    }
+    cout << "Monthly Payments: ";
     cin >> monthlyPayments; 
+    if (!cin){
+        throw invalid_argument("Input must be a integer");
+    }
 
 
+    cout << "**********************************************************" << endl;
+    cout << "       Amortization Table" << endl;
+    cout << "**********************************************************" << endl;
+    cout.setf(ios::left, ios::adjustfield);
+    cout << setw(7) << "Month" << setw(10) << "Balance" << setw(13) << "Payment" 
+    << setw(8) << "Rate" << setw(11) << "Interest" << setw(9) << "Principal" << endl;
     return 0;
 }
