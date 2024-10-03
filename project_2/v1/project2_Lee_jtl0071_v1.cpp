d16ed23d6ce25864d8b5216c58b2f32492c104c1
Jordan Lee <jtlee@rmgj2.com>
2024-10-03 14:35:20 -0500

    Changed chances to constants

--- project_2/v1/project2_Lee_jtl0071_v1.cpp
+++ project_2/v1/project2_Lee_jtl0071_v1.cpp
@@ -65,10 +65,11 @@ bool at_least_two_alive(bool A_alive, bool B_alive, bool C_alive)
  */
 void Aaron_shoots1(bool &B_alive, bool &C_alive)
 {
+    const int AARON_HIT_CHANCE = 33;
     // If both alive, Aaron has 1/3 chance of hitting Aaron
     if (B_alive && C_alive)
     {
-        if (randomNumberGenerator(33))
+        if (randomNumberGenerator(AARON_HIT_CHANCE))
         {
 
             C_alive = false;
@@ -78,7 +79,7 @@ void Aaron_shoots1(bool &B_alive, bool &C_alive)
     }
     if (B_alive && !C_alive)
     {
-        if (randomNumberGenerator(33))
+        if (randomNumberGenerator(AARON_HIT_CHANCE))
         {
             B_alive = false;
         }
@@ -87,7 +88,7 @@ void Aaron_shoots1(bool &B_alive, bool &C_alive)
     }
     if (!B_alive && C_alive)
     {
-        if (randomNumberGenerator(33))
+        if (randomNumberGenerator(AARON_HIT_CHANCE))
         {
 
             C_alive = false;
@@ -106,9 +107,10 @@ void Aaron_shoots1(bool &B_alive, bool &C_alive)
  */
 void Bob_shoots(bool &A_alive, bool &C_alive)
 {
+    const int BOB_HIT_CHANCE = 50;
     if (A_alive && C_alive)
     {
-        if (randomNumberGenerator(50))
+        if (randomNumberGenerator(BOB_HIT_CHANCE))
         {
             C_alive = false;
         }
@@ -117,7 +119,7 @@ void Bob_shoots(bool &A_alive, bool &C_alive)
     }
     if (A_alive && !C_alive)
     {
-        if (randomNumberGenerator(50))
+        if (randomNumberGenerator(BOB_HIT_CHANCE))
         {
 
             A_alive = false;
@@ -127,7 +129,7 @@ void Bob_shoots(bool &A_alive, bool &C_alive)
     }
     if (!A_alive && C_alive)
     {
-        if (randomNumberGenerator(50))
+        if (randomNumberGenerator(BOB_HIT_CHANCE))
         {
 
             C_alive = false;
@@ -166,6 +168,31 @@ void Charlie_shoots(bool &A_alive, bool &B_alive)
  */
 void Aaron_shoots2(bool &B_alive, bool &C_alive)
 {
+    const int AARON_HIT_CHANCE = 33;
+    // If both alive, Aaron has 1/3 chance of hitting Aaron
+    if (B_alive && C_alive)
+    {
+        return;
+    }
+    if (B_alive && !C_alive)
+    {
+        if (randomNumberGenerator(AARON_HIT_CHANCE))
+        {
+            B_alive = false;
+        }
+
+        return;
+    }
+    if (!B_alive && C_alive)
+    {
+        if (randomNumberGenerator(AARON_HIT_CHANCE))
+        {
+
+            C_alive = false;
+        }
+
+        return;
+    }
 }
 
 /**
@@ -240,38 +267,114 @@ int Strategy_1(bool &Aaron, bool &Bob, bool &Charlie)
     }
 }
 
+/**
+ * Strat 2: Aaron misses first shot. Identical to strategy 1 except we skip Aarons first shot.
+ *
+ * @param Aaron reference for Aaron bool
+ * @param Bob reference for Bob bool
+ * @param Charlie reference for Charlie bool
+ * @return 1 if Aaron wins, 2 if Bob wins, 3 if Charlie wins.
+ */
+int Strategy_2(bool &Aaron, bool &Bob, bool &Charlie)
+{
+
+    Aaron_shoots2(Bob,Charlie);
+    // Bob shoots at Charlie if Charlie is alive, otherwise he shoots at Aaron
+    if (Bob)
+    {
+        Bob_shoots(Aaron, Charlie);
+    }
+
+    // If Charlie is alive by now, Charlie shoots back at Bob, killing Bob
+    if (!at_least_two_alive(Aaron, Bob, Charlie))
+    {
+        if (Aaron)
+        {
+            return 1;
+        }
+        else if (Bob)
+        {
+            return 2;
+        }
+    }
+
+    if (Charlie)
+    {
+        Charlie_shoots(Aaron, Bob);
+        if (!at_least_two_alive(Aaron, Bob, Charlie))
+        {
+            return 3;
+        }
+    }
+
+    // If Aaron, then shoots at Charlie or Bob, with a 1/3 chance of hitting.
+    // If Bob, then shoots at Aaron or Charlie
+
+    if (Aaron)
+    {
+        Aaron_shoots2(Bob, Charlie);
+        if (!at_least_two_alive(Aaron, Bob, Charlie))
+        {
+            return 1;
+        }
+    }
+
+    if (Bob)
+    {
+        Bob_shoots(Aaron, Charlie);
+        if (!at_least_two_alive(Aaron, Bob, Charlie))
+        {
+            return 2;
+        }
+    }
+
+    if (Charlie)
+    {
+        Charlie_shoots(Aaron, Bob);
+        if (!at_least_two_alive(Aaron, Bob, Charlie))
+        {
+            return 3;
+        }
+    }
+
+}
+
 int main()
 {
     // Create boolean variables for each person, true if alive, false if dead.
-    bool Aaron = true;   // Hits 1/3 of shots
-    bool Bob = true;     // Hits 1/2 of shots
-    bool Charlie = true; // Always hits
+    bool Aaron;   // Hits 1/3 of shots
+    bool Bob;     // Hits 1/2 of shots
+    bool Charlie; // Always hits
     int A_count = 0;
     int B_count = 0;
     int C_count = 0;
-    double A_win_precent;
+    double A_win_precent_1;
+    double A_win_precent_2;
     double B_win_precent;
     double C_win_precent;
     const int ITERATION_COUNT = 10000;
-    srand(time(0));
+    srand((time(0))); // Had to use copilot to get rid of this 'implicit conversion loses integer precision' warning
 
     cout << "*** Welcome to Jordan's Truel of the Fates Simulator ***" << endl;
-    cout << "Ready to test strategy 1 (run " << ITERATION_COUNT << " times):";
-    cin.ignore().get();
 
-    cout << "Testing Strategy 1" << endl;
+    // Strategy 1
+    cout << "Ready to test strategy 1 (run " << ITERATION_COUNT << " times):" << endl;
+    cout << "Press any key to continue...";
+    cin.ignore().get();
+    cout << "Testing Strategy 1..." << endl;
+
     for (int i = 0; i < ITERATION_COUNT; i++)
     {
-        // Reset lives
-        bool Aaron = true;
-        bool Bob = true;
-        bool Charlie = true;
+        // Reset everyone to alive
+        Aaron = true;
+        Bob = true;
+        Charlie = true;
 
         // Find winner
-        int winner = Strategy_1(Aaron, Bob, Charlie);
+        int S1_winner = Strategy_1(Aaron, Bob, Charlie);
 
         // Add win to counts
-        switch (winner)
+        switch (S1_winner)
         {
         case 1:
             A_count++;
@@ -286,13 +389,64 @@ int main()
     }
 
     // Calculate Win Percentages
-    A_win_precent = (A_count / 10000.0) * 100;
+    A_win_precent_1 = (A_count / 10000.0) * 100;
     B_win_precent = (B_count / 10000.0) * 100;
     C_win_precent = (C_count / 10000.0) * 100;
 
     // Output counts.
-    cout << "Aaron won " << A_count << "/10000 truels or " << A_win_precent << "%" << endl;
+    cout << "Aaron won " << A_count << "/10000 truels or " << A_win_precent_1 << "%" << endl;
     cout << "Bob wins: " << B_count << "/10000 truels or " << B_win_precent << "%" << endl;
-    cout << "Charlie wins: " << C_count << "/10000 truels or " << C_win_precent << "%" << endl;
+    cout << "Charlie wins: " << C_count << "/10000 truels or " << C_win_precent << "%" << endl
+         << endl;
+
+    // Reset vars
+    B_win_precent = 0;
+    C_win_precent = 0;
+    A_count = 0;
+    B_count = 0;
+    C_count = 0;
+
+    // Strategy 2
+    cout << "Ready to test strategy 2 (run " << ITERATION_COUNT << " times):" << endl;
+    cout << "Press any key to continue...";
+    cin.ignore().get();
+
+    for (int i = 0; i < ITERATION_COUNT; i++)
+    {
+        // Reset everyone to alive
+        Aaron = true;
+        Bob = true;
+        Charlie = true;
+
+        // Find winner
+        int S2_winner = Strategy_2(Aaron, Bob, Charlie);
+
+        // Add win to counts
+        switch (S2_winner)
+        {
+        case 1:
+            A_count++;
+            break;
+        case 2:
+            B_count++;
+            break;
+        case 3:
+            C_count++;
+            break;
+        }
+    }
+
+    // Calculate Win Percentages
+    A_win_precent_2 = (A_count / 10000.0) * 100;
+    B_win_precent = (B_count / 10000.0) * 100;
+    C_win_precent = (C_count / 10000.0) * 100;
+
+    // Output counts
+    cout << "Aaron won " << A_count << "/10000 truels or " << A_win_precent_2 << "%" << endl;
+    cout << "Bob wins: " << B_count << "/10000 truels or " << B_win_precent << "%" << endl;
+    cout << "Charlie wins: " << C_count << "/10000 truels or " << C_win_precent << "%" << endl
+         << endl;
+
+    cout << (A_win_precent_1 > A_win_precent_2 ? "Strategy 1 is better than strategy 2." : "Strategy 2 is better than strategy 1.") << endl;
     return 0;
 }
