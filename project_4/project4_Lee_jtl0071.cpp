b0d8037387d487842d8fc8d462f4faa3389b20be
Jordan <jtlee@rmgj2.com>
2024-11-02 22:47:14 -0500

    Boilerplate for project4

---
+++ project_4/project4_Lee_jtl0071.cpp
@@ -0,0 +1,16 @@
+/*
+Jordan Lee
+jtl0071
+project4_Lee_jtl0071_v1.cpp
+
+Compile and Run Instructions:
+compile 'g++ project4_Lee_jtl0071.cpp -o project4_Lee_jtl0071.out'
+execute: './project4_Lee_jtl0071.out'
+*/
+
+using namespace std;
+
+int main()
+{
+
+}
